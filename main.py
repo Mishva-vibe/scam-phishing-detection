c537231926050c29558bc77706e47d1150a14540
Mishva Patel <mishvapatel75@gmail.com>
2026-09-19 21:59:30 +0530

    Add check for excessive dots in URL

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ if "@" in url:
     print("Warning: Suspicious URL")
 elif len(url) > 75:
     print("Warning: URL is too long")
+elif url.count(".") > 3:
+    print("Warning: Too many dots in URL")
 else:
     print("URL looks normal")
