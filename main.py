dcc668b1d307a606d4a979377a1d9845ab7657c9
Mishva Patel <mishvapatel75@gmail.com>
2026-09-21 20:23:52 +0530

    Refactor URL validation to use a scoring system

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 url = input("Enter URL: ")
 
+score = 0
+
 if "@" in url:
-    print("Warning: Suspicious URL")
-elif not url.startswith("https://"):
-    print("Warning: URL is not secure")
-elif any(word in url.lower() for word in ["login", "verify", "account", "bank", "password"]):
-    print("Warning: Suspicious words found in URL")
-elif len(url) > 75:
-    print("Warning: URL is too long")
-elif url.count(".") > 3:
-    print("Warning: Too many dots in URL")
+    score += 1
+
+if not url.startswith("https://"):
+    score += 1
+
+if any(word in url.lower() for word in ["login", "verify", "account", "bank", "password"]):
+    score += 1
+
+if len(url) > 75:
+    score += 1
+
+if url.count(".") > 3:
+    score += 1
+
+print("Phishing Score:", score)
+
+if score >= 3:
+    print("Warning: URL may be suspicious")
 else:
     print("URL looks normal")
