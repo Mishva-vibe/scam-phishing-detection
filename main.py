ce5de234c18973f3d4ba8e1bfb412bccc36a1cdd
Mishva Patel <277733396+Mishva-vibe@users.noreply.github.com>
2026-09-22 21:47:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
+from flask import Flask, render_template, request
 import re
 
-url = input("Enter URL: ")
+app = Flask(__name__)
 
-score = 0
+@app.route("/", methods=["GET", "POST"])
+def home():
+    score = None
+    url = ""
 
-if "@" in url:
-    score += 1
+    if request.method == "POST":
+        url = request.form["url"]
+        score = 0
 
-if not url.startswith("https://"):
-    score += 1
+        if "@" in url:
+            score += 1
 
-if any(word in url.lower() for word in ["login", "verify", "account", "bank", "password"]):
-    score += 1
+        if not url.startswith("https://"):
+            score += 1
 
-if len(url) > 75:
-    score += 1
+        if any(word in url.lower() for word in ["login", "verify", "account", "bank", "password"]):
+            score += 1
 
-if url.count(".") > 3:
-    score += 1
+        if len(url) > 75:
+            score += 1
 
-if re.search(r"https?://\d+\.\d+\.\d+\.\d+", url):
-    score += 1
+        if url.count(".") > 3:
+            score += 1
 
-percentage = (score / 6) * 100
+        if re.search(r"https?://\d+\.\d+\.\d+\.\d+", url):
+            score += 1
 
-print("Phishing Risk:", percentage, "%")
+        score = (score / 6) * 100
 
-if percentage >= 60:
-    print("Warning: URL may be suspicious")
-else:
-    print("URL looks normal")
+    return render_template("index.html", score=score, url=url)
 
+if __name__ == "__main__":
+    app.run(debug=True)
